a2829407c0bc22b9723917339a6e33306cb14362
Mael Valin <votre.email@example.com>
2026-01-27 14:57:58 +0100

    rotation

--- client/weapons/pistolet.js
+++ client/weapons/pistolet.js
@@ -92,7 +92,7 @@ AFRAME.registerComponent('pistolet-shooter', {
 			if (this.hand) {
 				// Ajustements spécifiques pour VR : plus petit et rotation Y=180, Z=90
 				this.modelEl.setAttribute('scale', '0.7 0.7 0.7');
-				this.modelEl.setAttribute('rotation', '0 180 90');
+				this.modelEl.setAttribute('rotation', '0 0 0');
 				this.hand.appendChild(this.modelEl);
 				// attach controller events immediately
 				if (this.hand.addEventListener) {
